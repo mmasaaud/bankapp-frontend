2d3a1a48a56ef7c4148945100df605c95a9caa0f
mmasaaud <93669379+mmasaaud@users.noreply.github.com>
2022-01-14 15:07:04 -0600

    change the title

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <p>
-            Welcome to AWS Bank
+            Welcome to Mike Bank
           </p>
         </header>  
           <Routes>
