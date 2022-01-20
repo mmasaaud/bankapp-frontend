a14adcb4880012433160f2266bef8b7256578e95
mmasaaud <93669379+mmasaaud@users.noreply.github.com>
2022-01-20 13:18:02 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <p>
-            Welcome to Octank Bank
+            Welcome to Oct Bank
           </p>
         </header>  
           <Routes>
